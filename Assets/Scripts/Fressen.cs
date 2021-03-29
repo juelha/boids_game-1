a582ee59790ab316cd507d8ffefd88e734915116
Magnus Müller <mamagnus00@gmail.com>
2021-03-29 08:21:18 +0200

    Ende
    End game if you press esc or when timer is 0, but just in build version, not in editor

--- Assets/Scripts/Fressen.cs
+++ Assets/Scripts/Fressen.cs
@@ -22,15 +22,25 @@ public class Fressen : MonoBehaviour
     {
         //Update Score
         scoreText.text = "Score: " + score.ToString();
+
+        //Countdown
         timeText.text = (int)gametime + "s";
         gametime -= Time.deltaTime;
+        //Min should be 0
+        gametime = Mathf.Max(0, gametime);
 
+        //Stop Game if gametime == 0 or if esc
+        if (gametime <= 0 || Input.GetButton("Cancel"))
+        {
+            //Debug.Log("Ende");
+            //Not in the Editor, just in build version
+            Application.Quit();
+        }
     }
 
     private void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "boid");// || col.gameObject.tag == "wall2" || col.gameObject.tag == "wall3" || col.gameObject.tag == "wall4" )
-
+        if (col.gameObject.tag == "boid") // || col.gameObject.tag == "wall2" || col.gameObject.tag == "wall3" || col.gameObject.tag == "wall4" )
         {
             score += 1;
             Destroy(col.gameObject);
