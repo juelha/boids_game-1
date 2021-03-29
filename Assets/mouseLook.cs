946107d1e02b50ade2873555ac784421d2f1b0fb
Magnus Müller <mamagnus00@gmail.com>
2021-03-29 07:04:36 +0200

    Fix Rotation
    Cant rotate around z Axis anymore,
    Clip x Rotation to +- 45 degree
    
    Now we have natural movement

--- Assets/mouseLook.cs
+++ Assets/mouseLook.cs
@@ -6,7 +6,7 @@ public class mouseLook : MonoBehaviour
 {
     public float mouseSensitivity = 200f;
     public Transform playerBody;
-    //float xRotation = 0f;
+    float xRotation = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,19 +18,42 @@ public class mouseLook : MonoBehaviour
     {
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        //playerBody.Rotate(Vector3.up * mouseX + Vector3.left * mouseY );
 
-        //xRotation -= mouseY;
+        xRotation = -mouseY;
         //xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
+        
+        //Debug.Log(xRotation);
         //transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
 
-        //playerBody.Rotate(Vector3.right * mouseX);
+        playerBody.Rotate(Vector3.right * xRotation);
+        playerBody.Rotate(Vector3.up * mouseX);
+
+
+        //set z rotation to 0, for better gameplay
+        var rotationVector = playerBody.transform.rotation.eulerAngles;
+        rotationVector.z = 0f;
+
+        //set x rotation for natural movement
+        if(rotationVector.x < 180 && rotationVector.x > 45)
+        {
+            rotationVector.x = 45f;
+        }
+        if (rotationVector.x < 315 && rotationVector.x > 180)
+        {
+            rotationVector.x = 315f;
+        }
+        Debug.Log(rotationVector.x);
+        playerBody.transform.rotation = Quaternion.Euler(rotationVector);
+
+
+        //playerBody.transform.localRotation.eulerAngles = 0f;
         //
         //
         //playerBody.Rotate(Vector3.* mouseY);
 
         //playerBody.LookAt(new Vector3(mouseX,))
         //Debug.Log(Vector3.up * mouseX + Vector3.left * mouseY);
-        playerBody.Rotate(Vector3.up * mouseX + Vector3.left * mouseY );
+
     }
 }
