0b122bdc067279f28c6595eefebcb1a84e18426b
juelha <59289684+juelha@users.noreply.github.com>
2021-04-01 07:56:16 +0200

    figured out how to marry JOBS and gameobjs
    wrote spawner in manager
    need to build API that can communicate between JOB and Boid.cs

--- Assets/Scripts/BoidsJobified/BoidManager.cs
+++ Assets/Scripts/BoidsJobified/BoidManager.cs
@@ -7,12 +7,32 @@ using Unity.Collections;
 public class BoidManager : MonoBehaviour {
 
 
-    [SerializeField] private List<Boid> boids;
+    private List<GameObject> boids;
     private NativeArray<Boid.Data> _boidsDataArray;
     private BoidUpdateJob _job;
     //private BoidMoveJob move_job;
 
+    public GameObject prefab;
+    public float radius;
+    public int number;
+
+    
+
+    // Spawner 
+    // Start is called before the first frame update
+    void Start() {
+        for (int i = 0; i < number; ++i) {
+            
+            // anything inside of sphere is going to be a valid spawn location 
+            //                                           sphere at 0/0/0 with r=1           
+            boids.Add(Instantiate(prefab, this.transform.position + Random.insideUnitSphere * radius, Random.rotation, this.transform));
+       
+        }
+    }
+
     private void Awake() {
+
+        // MAKE API HERE
         // include a boid data array based on its list of buildings from the scene
         //                                           length = size of list, Allocator (let jobs know how long we need this list to live in memory) 
         _boidsDataArray = new NativeArray<Boid.Data>(boids.Count, Allocator.Persistent);  // .TempJob <- only using this list for 1 frame within a single _job
@@ -20,13 +40,13 @@ public class BoidManager : MonoBehaviour {
         // populate the array
         for (var i = 0; i < boids.Count; i++) {
 
-            _boidsDataArray[i] = new Boid.Data(boids[i]);
+           // _boidsDataArray[i] = boids[i];
         }
 
         // pass it into BoidUpdateJob
         _job = new BoidUpdateJob {
 
-            BoidDataArray = _boidsDataArray
+            BoidDataArray = _boidsDataArray // PASS API HERE
         };
     }
 
---
+++ Assets/Scripts/Spawners/BoidSpawnerJobified.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Entities;
+
+
+
+
+
+public struct BoidSpawnerJobified : IComponentData
+{
+    public Entity prefab;
+
+}
