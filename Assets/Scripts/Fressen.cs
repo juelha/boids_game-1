ce4f9c4350c0c763da0402437ab229c0290eec8a
Tobi Obeck <13554426+TobiObeck@users.noreply.github.com>
2021-04-02 20:39:13 +0200

    replaces UI Texts with TextMeshPro
    The white texts in the UI have now a dark dropshadow, so that they can be read more easily.
    
    Also major refactor of Fressen.cs

--- Assets/Scripts/Fressen.cs
+++ Assets/Scripts/Fressen.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using System;
+
 public class Fressen : MonoBehaviour
 {
     [SerializeField]
@@ -10,9 +13,9 @@ public class Fressen : MonoBehaviour
 
     int score = 0;
     public float gametime = 60f;
-    public Text scoreText;
-    public Text timeText;
-    public Text speed;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI countdownTimeText;
+    public TextMeshProUGUI speed;
 
     public float maxSize = 50f;
     public float minSize = 3f;
@@ -24,47 +27,64 @@ public class Fressen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        score = 0;
-
-
+        this.score = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Update Score
-        scoreText.text = "Score: " + score.ToString();
+        this.UpdateGameTime();
 
-        //Countdown
-        timeText.text = (int)gametime + "s";
-        gametime -= Time.deltaTime;
-        //Min should be 0
-        gametime = Mathf.Max(0, gametime);
+        this.DisplayScoreUI();
+        this.DisplayCountdownTimeUI();
+        this.DisplaySpeedUI();
 
-        //display speed
-        speed.text = player.speed.ToString("F2");
+        // this.ShrinkShark();
+
+        if (this.IsGameOver())
+        {
+            // TODO CHANGE GLOBAL GAME STATE HERE
+            throw new NotImplementedException("TIME IS OVER! TODO CHANGE GAME STATE");
+        }
+    }
+
+    private void UpdateGameTime()
+    {
+        this.gametime -= Time.deltaTime;
+        this.gametime = Mathf.Max(0, gametime); // Min should be 0
+    }
+
+    private bool IsGameOver()
+    {
+        return this.gametime <= 0 || Input.GetButton("Cancel"); // "Cancel" means Escape key
+    }
 
 
-        //Make shark smaller with time
+    private void ShrinkShark()
+    {
+        // Make shark smaller with time
         transform.localScale -= new Vector3(1f, 1f, 0f) * decreaseSize;
         if (transform.localScale.x < minSize)
         {
             transform.localScale = new Vector3(minSize, minSize, transform.localScale.z);
             //ende!
         }
+    }
 
+    private void DisplayScoreUI()
+    {
+        this.scoreText.text = "Score: " + score.ToString();
+    }
 
+    private void DisplayCountdownTimeUI()
+    {
+        TimeSpan timeSpan = TimeSpan.FromSeconds((int)gametime);
+        this.countdownTimeText.text = timeSpan.ToString("m':'ss");
+    }
 
-
-        //Stop Game if gametime == 0 or if esc
-        if (gametime <= 0 || Input.GetButton("Cancel"))
-        {
-            //Debug.Log("Ende");
-            //Not in the Editor, just in build version
-            Application.Quit();
-        }
-
-
+    private void DisplaySpeedUI()
+    {
+        this.speed.text = player.speed.ToString("F2");
     }
 
     private void OnCollisionEnter(Collision col)
@@ -72,27 +92,20 @@ public class Fressen : MonoBehaviour
     {
         if (col.gameObject.tag == "Boid") // || col.gameObject.tag == "wall2" || col.gameObject.tag == "wall3" || col.gameObject.tag == "wall4" )
         {
-            //Increase shark, when he eats fish, but with maximum size 
+            //Increase shark, when he eats fish, but with maximum size
             transform.localScale += new Vector3(1f, 1f, 0f) * increaseSize;
             if (transform.localScale.x > maxSize)
             {
                 transform.localScale = new Vector3(maxSize, maxSize, transform.localScale.z);
             }
 
-            //Debug.Log(score);
-            score += 1;
+            this.score += 1;
 
-
-            //Collision coll_copy = col;
-
-            //Destry and Highlight boid
+            //Destroy
             DestroyBoidAfter(3f, col);
-            //StartCoroutine(DestroyBoidAfterTime(1f, coll_copy)); 
-            //Destroy(col.gameObject);
 
             //slow down when eat fish
-            player.slowDown();
-
+            // player.slowDown();
         }
     }
     private void DestroyBoidAfter(float time, Collision coll)
@@ -110,40 +123,8 @@ public class Fressen : MonoBehaviour
 
         //Set new Parent = Player
         coll.gameObject.transform.SetParent(player.gameObject.transform);
-        
+
         Destroy(coll.gameObject, time);
 
-
     }
-    /*
-    IEnumerator DestroyBoidAfterTime(float time, Collision coll)
-    {
-        Material boid = coll.gameObject.GetComponentInChildren<Renderer>().material;
-        //Debug.Log(boid);
-        boid.SetColor("_Color", Color.red);
-
-        //Debug.Log(col.gameObject);
-
-        //Destroy(coll.gameObject.GetComponent<BoidAlignment>());
-        //Destroy(coll.gameObject.GetComponent<BoidCohesion>());
-        //Destroy(coll.gameObject.GetComponent<BoidContainerBehavior>());
-        //Destroy(coll.gameObject.GetComponent<BoidSeparation>());
-        //Destroy(coll.gameObject.GetComponent<Boid>());
-        //amFressen = true;
-        Debug.Log("Destroying " + coll.gameObject.GetInstanceID());
-        yield return new WaitForSeconds(time);
-
-
-        //Prevent wrong destroing
-        if(coll.gameObject.tag != "Player")
-        {
-            Debug.Log("Destroying " + coll.gameObject.GetInstanceID());
-
-            Destroy(coll.gameObject);
-
-
-        }
-        //amFressen = false;
-    }
-    */
 }
