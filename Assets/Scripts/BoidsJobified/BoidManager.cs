900768bedf6105b4226981303797bd82f41baccb
juelha <59289684+juelha@users.noreply.github.com>
2021-04-03 18:43:04 +0200

    progress with JOBS and GameObjects marrying
    stuck on "A Native Collection has not been disposed, resulting in a memory leak. Enable Full StackTraces to get more details."

--- Assets/Scripts/BoidsJobified/BoidManager.cs
+++ Assets/Scripts/BoidsJobified/BoidManager.cs
@@ -2,66 +2,111 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.Jobs;
 using Unity.Collections;
+using UnityEngine.Jobs;
 
 
 public class BoidManager : MonoBehaviour {
 
 
-    private List<GameObject> boids;
-    private NativeArray<Boid.Data> _boidsDataArray;
-    private BoidUpdateJob _job;
+    public List<GameObject> boids;
+    public GameObject boid; 
+    //private NativeArray<Boid.Data> _boidsDataArray;
+    //private BoidUpdateJob _UpdateJob;
     //private BoidMoveJob move_job;
 
     public GameObject prefab;
     public float radius;
     public int number;
 
-    
+    //
+    NativeArray<Vector3> Velocities;
+    TransformAccessArray TransformAccessArray;
+
+    // what type???
+    GameObject[] GOArray;
+    //  public List<Transform> TransformTemp;
+    public Transform[] TransformTemp;// = new Transform[100];
+
+    public BoidUpdateJob _UpdateJob;
+    JobHandle UpdateJobHandle;
+
+
 
     // Spawner 
     // Start is called before the first frame update
     void Start() {
         for (int i = 0; i < number; ++i) {
-            
+
             // anything inside of sphere is going to be a valid spawn location 
             //                                           sphere at 0/0/0 with r=1           
-            boids.Add(Instantiate(prefab, this.transform.position + Random.insideUnitSphere * radius, Random.rotation, this.transform));
-       
+            boid = new GameObject();
+          //  var bla = boid.transform; 
+            boid = Instantiate(prefab, Random.insideUnitSphere * radius, Random.rotation);
+            boids.Add(boid);
+
+            Transform[] TransformTemp = new Transform[100];
+            TransformTemp[i] = boid.transform;
+
         }
+
+
+ 
+
+
+
+        TransformAccessArray = new TransformAccessArray(TransformTemp);
+
+
     }
 
-    private void Awake() {
+   // private void Awake() {
 
         // MAKE API HERE
         // include a boid data array based on its list of buildings from the scene
         //                                           length = size of list, Allocator (let jobs know how long we need this list to live in memory) 
-        _boidsDataArray = new NativeArray<Boid.Data>(boids.Count, Allocator.Persistent);  // .TempJob <- only using this list for 1 frame within a single _job
+       // _boidsDataArray = new NativeArray<Boid.Data>(boids.Count, Allocator.Persistent);  // .TempJob <- only using this list for 1 frame within a single _UpdateJob
 
         // populate the array
-        for (var i = 0; i < boids.Count; i++) {
+       // for (var i = 0; i < boids.Count; i++) {
 
            // _boidsDataArray[i] = boids[i];
-        }
+       // }
 
         // pass it into BoidUpdateJob
-        _job = new BoidUpdateJob {
+      //  _UpdateJob = new BoidUpdateJob {
 
-            BoidDataArray = _boidsDataArray // PASS API HERE
-        };
-    }
+        //    BoidDataArray = _boidsDataArray // PASS API HERE
+     //   };
+  //  }
 
     private void Update() {
-        // length (number of items the _job will iterate over)
+        // length (number of items the _UpdateJob will iterate over)
         // batchcount (size of each threaded batch)
         //        length = size of list, batchcount = 1    
-        var jobHandle = _job.Schedule(boids.Count, 1);
-              // this will get the _job running
+        //  var jobHandle = _UpdateJob.Schedule(transformsAccess);
+        // this will get the _UpdateJob running
 
-        jobHandle.Complete();  // ensure that _job finished its work before next frame 
+        //  var jobHandle = _UpdateJob.Schedule(transformsAccess);
+
+        //  jobHandle.Complete();  // ensure that _UpdateJob finished its work before next frame 
+
+        _UpdateJob = new BoidUpdateJob() {
+            deltaTime = Time.deltaTime,
+            velocity = Velocities,
+        };
+
+
+        UpdateJobHandle = _UpdateJob.Schedule(TransformAccessArray);
+    }
+
+
+
+    public void LateUpdate() {
+        UpdateJobHandle.Complete();
     }
 
     private void OnDestroy() {
-        _boidsDataArray.Dispose(); // need to delete array manually
+        Velocities.Dispose();
+        TransformAccessArray.Dispose();
     }
-
 }
--- Assets/Scripts/BoidsJobified/BoidUpdateJob.cs
+++ Assets/Scripts/BoidsJobified/BoidUpdateJob.cs
@@ -6,29 +6,46 @@ using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
 using UnityEngine.Jobs;
+using Unity.Mathematics;
 
 
 [BurstCompile]
 
 // create a job
-public struct BoidUpdateJob : IJobParallelFor {  // IJobParallelFor can run same logic over a list of items
+public struct BoidUpdateJob : IJobParallelForTransform {  // IJobParallelFor can run same logic over a list of items
 
     // add array of dataobjs for job to iterate over
-    public NativeArray<Boid.Data> BoidDataArray;  // why NativeArray? <-  special type of array that was designed to work optimally with jobs
+    // public NativeArray<Boid.Data> BoidDataArray;  // why NativeArray? <-  special type of array that was designed to work optimally with jobs
 
-    public void Execute(int index) {
+    public NativeArray<Vector3> velocity;  // the velocities from AccelerationJob
+
+    public float deltaTime;
+    public Vector3 vel;
+    
+
+    public void Execute(int i, TransformAccess transform) {
+
+
+        vel = new Vector3(1, 1, 0);
+        // move the given transforms forward in their local coordinate system
+        //  transform.localPosition += transform.localRotation * math.float3(0, 0, 1);
+
+        transform.position += vel * deltaTime;
+    }
+
+   // public void Execute(int index) {
         // how it works:
         // 1. pull dataObj out of array
         // 2. do our work
         // 3. put it back in
 
-        var curDataObj = BoidDataArray[index];  // 1. ref to current data obj 
-        curDataObj.Update();                    // 2. do stuff
+     //   var curDataObj = BoidDataArray[index];  // 1. ref to current data obj 
+    //    curDataObj.Update();                    // 2. do stuff
 
 
 
-        BoidDataArray[index] = curDataObj;      // 3. pass the data back in the array using index
-    }
+       // BoidDataArray[index] = curDataObj;      // 3. pass the data back in the array using index
+   // }
 
 }
 
