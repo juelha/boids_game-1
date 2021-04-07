12be6942e6ae994e3ba74d60d11ca1ff8f237dd9
Tara Schuchort <64102128+tara-schuchort@users.noreply.github.com>
2021-04-07 15:01:57 +0200

    Changed y-axis barrier for player movement
    Changed y-axis barrier for player movement from 7.9 to 7.5
    Also changed any German comments to English
    Removed useless whitespace in script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -11,7 +11,6 @@ public class PlayerMovement : MonoBehaviour
     public float waitForNextSpeed = 5f;
     private bool isFast = false;
     // Start is called before the first frame update
-    //public float sensibility;
 
     void Start()
     {
@@ -23,14 +22,12 @@ public class PlayerMovement : MonoBehaviour
     {
         //Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
         //float midPoint = (transform.position - Camera.main.transform.position).magnitude * 0.5f;
-        //
         //transform.LookAt(mouseRay.origin * sensibility  + mouseRay.direction * midPoint);
-
 
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        //verhindern, dass er rückwärts kann
+        // if Player would move backwards (z > 0), stop movement -> shark can't swim backwards
         if (z < 0) z = 0;
 
         Vector3 move = transform.right * x + transform.forward * z;
@@ -38,10 +35,10 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
 
         // check for upper boundary (water surface, currently at y = 8.5)
-        if (transform.position.y > 7.9f)
+        if (transform.position.y > 7.5f)
         {
-            // keep player position at 8.4
-            transform.position = new Vector3(transform.position.x, 7.9f, transform.position.z);
+            // keep player position and outer camera under the water surface 
+            transform.position = new Vector3(transform.position.x, 7.5f, transform.position.z);
         }
 
         //If u press space, speed up
@@ -54,7 +51,6 @@ public class PlayerMovement : MonoBehaviour
     //Speed up
     public void speedUp()
     {
-
         StartCoroutine(speedUpTime(speedUpFor, waitForNextSpeed));
     }
 
@@ -66,10 +62,7 @@ public class PlayerMovement : MonoBehaviour
         speed /= 2;
         yield return new WaitForSeconds(wait);
         isFast = false;
-
-
     }
-
 
     //Slow down function
     IEnumerator slowDownTime(float time)
@@ -83,7 +76,6 @@ public class PlayerMovement : MonoBehaviour
     public void slowDown()
     {
         StartCoroutine(slowDownTime(slowDownFor));
-
     }
 
 
